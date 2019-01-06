c62de1f0023228a1bc8f5ca9bd183083ade87905
sidhom <1qw23er4>
2019-01-06 16:48:10 -0500

    bug_fixes

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Render the notebook to HTML
+jupyter nbconvert \
+  --ExecutePreprocessor.allow_errors=True \
+  --ExecutePreprocessor.timeout=-1 \
+  --output-dir = /results \
+  --execute Tutorial.ipynb
