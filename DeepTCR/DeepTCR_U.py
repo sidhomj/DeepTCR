791765129b2fcfca1829336b3809d47b04839e4f
sidhom <1qw23er4>
2019-03-19 13:17:44 -0400

    new_features

--- DeepTCR/DeepTCR_U.py
+++ DeepTCR/DeepTCR_U.py
@@ -1259,7 +1259,6 @@ class DeepTCR_U(object):
             RGB_tuples = map(lambda x: colorsys.hsv_to_rgb(*x), HSV_tuples)
             color_dict = dict(zip(np.unique(self.label_id), RGB_tuples))
 
-
         rad_plot(X_2,pairwise_distances,samples,labels,self.file_id,color_dict,
                  gridsize=gridsize,dg_radius=dendrogram_radius,linkage_method=linkage_method,
                  figsize=8,axes_radius=repertoire_radius)
