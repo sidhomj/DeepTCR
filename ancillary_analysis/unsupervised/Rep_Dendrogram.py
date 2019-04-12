59c4e92e19359394786144ab2d58019942eaa639
JW <sidhomj@gmail.com>
2019-04-12 17:15:49 -0400

    bug_fix

--- ancillary_analysis/unsupervised/Rep_Dendrogram.py
+++ ancillary_analysis/unsupervised/Rep_Dendrogram.py
@@ -8,8 +8,8 @@ DTCRU.Get_Data(directory='../../Data/Rudqvist',Load_Prev_Data=False,aggregate_by
                aa_column_beta=1,count_column=2,v_beta_column=7,d_beta_column=14,j_beta_column=21)
 
 #Train VAE
-DTCRU.Train_VAE()
+DTCRU.Train_VAE(accuracy_min=0.9)
 color_dict = {'Control':'limegreen','9H10':'red','RT':'darkorange','Combo':'magenta'}
 DTCRU.Repertoire_Dendrogram(n_jobs=40,distance_metric='KL',
-                           dendrogram_radius=0.28,repertoire_radius=0.35,Load_Prev_Data=True,gridsize=5,
+                           dendrogram_radius=0.28,repertoire_radius=0.35,Load_Prev_Data=True,gridsize=6,
                             color_dict=color_dict)
