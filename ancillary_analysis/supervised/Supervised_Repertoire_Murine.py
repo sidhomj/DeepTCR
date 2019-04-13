3d38bd8b8d8086907bea78a35cfb6853260790aa
JW <sidhomj@gmail.com>
2019-04-13 01:17:46 -0400

    bug_fix

---
+++ ancillary_analysis/supervised/Supervised_Repertoire_Murine.py
@@ -0,0 +1,18 @@
+from DeepTCR.DeepTCR import DeepTCR_SS, DeepTCR_WF
+
+#Train Sequence Classifier
+DTCR_SS = DeepTCR_SS('Rudqvist')
+DTCR_SS.Get_Data(directory='../../Data/Rudqvist',Load_Prev_Data=False,aggregate_by_aa=True,
+               aa_column_beta=1,count_column=2,v_beta_column=7,d_beta_column=14,j_beta_column=21)
+
+DTCR_SS.Monte_Carlo_CrossVal(folds=100,test_size=0.25)
+DTCR_SS.AUC_Curve()
+
+#Train Repertoire Classifier
+DTCR_WF = DeepTCR_WF('Rudqvist')
+DTCR_WF.Get_Data(directory='../../Data/Rudqvist',Load_Prev_Data=False,aggregate_by_aa=True,
+               aa_column_beta=1,count_column=2,v_beta_column=7,d_beta_column=14,j_beta_column=21)
+
+DTCR_SS.Monte_Carlo_CrossVal(folds=100,LOO=4)
+DTCR_SS.AUC_Curve()
+
