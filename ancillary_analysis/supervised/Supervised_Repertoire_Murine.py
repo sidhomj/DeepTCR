52277f8ce4f66b4bf08da4a5d78b0d35f066c590
JW <sidhomj@gmail.com>
2019-04-13 14:22:11 -0400

    bug_fix

--- ancillary_analysis/supervised/Supervised_Repertoire_Murine.py
+++ ancillary_analysis/supervised/Supervised_Repertoire_Murine.py
@@ -8,11 +8,15 @@ DTCR_SS.Get_Data(directory='../../Data/Rudqvist',Load_Prev_Data=False,aggregate_
 DTCR_SS.Monte_Carlo_CrossVal(folds=100,test_size=0.25)
 DTCR_SS.AUC_Curve()
 
-#Train Repertoire Classifier
+#Train Repertoire Classifier without on-graph clustering
 DTCR_WF = DeepTCR_WF('Rudqvist')
 DTCR_WF.Get_Data(directory='../../Data/Rudqvist',Load_Prev_Data=False,aggregate_by_aa=True,
                aa_column_beta=1,count_column=2,v_beta_column=7,d_beta_column=14,j_beta_column=21)
 
-DTCR_SS.Monte_Carlo_CrossVal(folds=100,LOO=4)
-DTCR_SS.AUC_Curve()
+DTCR_WF.Monte_Carlo_CrossVal(folds=100,LOO=4)
+DTCR_WF.AUC_Curve()
+
+#Train Repertoire Classifier with on-graph clustering
+DTCR_WF.Monte_Carlo_CrossVal(folds=100,LOO=4,on_graph_clustering=True)
+DTCR_WF.AUC_Curve()
 
