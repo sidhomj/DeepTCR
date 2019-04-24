5f335b8733c0168221e43fa5c5362a1f3ce03908
JW <sidhomj@gmail.com>
2019-04-24 18:12:11 -0400

    bug_fix

--- ancillary_analysis/supervised/Supervised_Repertoire_Human.py
+++ ancillary_analysis/supervised/Supervised_Repertoire_Human.py
@@ -1,7 +1,7 @@
 from DeepTCR.DeepTCR import DeepTCR_WF
 
 #Train Sequence Classifier
-DTCR = DeepTCR_WF('Human_TIL')
+DTCR = DeepTCR_WF('Human_TIL',device='/gpu:2')
 dir = 'Topalian/beta/pre_crpr_sdpd'
 DTCR.Get_Data(directory='../../Data/Topalian',Load_Prev_Data=False,
                aa_column_beta=1,count_column=2,v_beta_column=7,d_beta_column=14,j_beta_column=21,data_cut=0.25,
@@ -17,18 +17,17 @@ size_of_net = 'small'
 DTCR.use_hla = False
 DTCR.Monte_Carlo_CrossVal(folds=folds,LOO=LOO,epochs_min=epochs_min,
                           use_only_hla=False,weight_by_class=weight_by_class,size_of_net=size_of_net)
-DTCR.AUC_Curve()
+DTCR.AUC_Curve(filename='AUC_Seq.eps')
 
 #Just train w/HLA
 DTCR.use_hla = True
 DTCR.Monte_Carlo_CrossVal(folds=folds,LOO=LOO,epochs_min=epochs_min,
                           use_only_hla=True,weight_by_class=weight_by_class,size_of_net=size_of_net)
-DTCR.AUC_Curve()
+DTCR.AUC_Curve(filename='AUC_HLA.eps')
 
 #Train with both Seq + HLA
 DTCR.Monte_Carlo_CrossVal(folds=folds,LOO=LOO,epochs_min=epochs_min,
                           use_only_hla=False,weight_by_class=weight_by_class,size_of_net=size_of_net)
-
-DTCR.AUC_Curve()
+DTCR.AUC_Curve(filename='AUC_Seq_HLA.eps')
 
 
--- ancillary_analysis/supervised/Supervised_Repertoire_Murine.py
+++ ancillary_analysis/supervised/Supervised_Repertoire_Murine.py
@@ -2,7 +2,7 @@ from DeepTCR.DeepTCR import DeepTCR_SS, DeepTCR_WF
 
 folds = 100
 LOO = 4
-epochs_min = 50
+epochs_min = 100
 
 #Train Sequence Classifier
 DTCR_SS = DeepTCR_SS('Rudqvist_SS')
@@ -18,11 +18,11 @@ DTCR_WF.Get_Data(directory='../../Data/Rudqvist',Load_Prev_Data=False,
                aa_column_beta=1,count_column=2,v_beta_column=7,d_beta_column=14,j_beta_column=21)
 
 DTCR_WF.Monte_Carlo_CrossVal(folds=folds,LOO=LOO,epochs_min=epochs_min)
-DTCR_WF.AUC_Curve()
+DTCR_WF.AUC_Curve(filename='Rep_AUC.eps')
 
 #Train Repertoire Classifier with on-graph clustering
-DTCR_WF.Monte_Carlo_CrossVal(folds=1,LOO=LOO,on_graph_clustering=True,epochs_min=epochs_min)
-DTCR_WF.AUC_Curve()
+DTCR_WF.Monte_Carlo_CrossVal(folds=folds,LOO=LOO,on_graph_clustering=True,epochs_min=epochs_min)
+DTCR_WF.AUC_Curve(filename='Rep_AUC_clustering.eps')
 
 #Visualize Latent Space
 DTCR_WF.UMAP_Plot(by_class=True,freq_weight=True,show_legend=True,scale=5000,Load_Prev_Data=False,
