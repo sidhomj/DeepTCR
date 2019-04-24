8c2b4a11932002be8165e5e6b35d938ab9f4d836
JW <sidhomj@gmail.com>
2019-04-23 20:07:33 -0400

    bug_fix

--- ancillary_analysis/supervised/Supervised_Repertoire_Murine.py
+++ ancillary_analysis/supervised/Supervised_Repertoire_Murine.py
@@ -6,7 +6,7 @@ epochs_min = 50
 
 #Train Sequence Classifier
 DTCR_SS = DeepTCR_SS('Rudqvist')
-DTCR_SS.Get_Data(directory='../../Data/Rudqvist',Load_Prev_Data=False,aggregate_by_aa=True,
+DTCR_SS.Get_Data(directory='../../Data/Rudqvist',Load_Prev_Data=False,
                aa_column_beta=1,count_column=2,v_beta_column=7,d_beta_column=14,j_beta_column=21)
 
 DTCR_SS.Monte_Carlo_CrossVal(folds=folds,test_size=0.25)
@@ -14,7 +14,7 @@ DTCR_SS.AUC_Curve()
 
 #Train Repertoire Classifier without on-graph clustering
 DTCR_WF = DeepTCR_WF('Rudqvist')
-DTCR_WF.Get_Data(directory='../../Data/Rudqvist',Load_Prev_Data=False,aggregate_by_aa=True,
+DTCR_WF.Get_Data(directory='../../Data/Rudqvist',Load_Prev_Data=False,
                aa_column_beta=1,count_column=2,v_beta_column=7,d_beta_column=14,j_beta_column=21)
 
 DTCR_WF.Monte_Carlo_CrossVal(folds=folds,LOO=LOO,epochs_min=epochs_min)
