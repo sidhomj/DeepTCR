934847199a28a1867c71361e97b3f7eb1482b1ee
JW <sidhomj@gmail.com>
2019-04-24 16:55:01 -0400

    bug_fix

--- ancillary_analysis/supervised/Supervised_Repertoire_Human.py
+++ ancillary_analysis/supervised/Supervised_Repertoire_Human.py
@@ -1,7 +1,7 @@
 from DeepTCR.DeepTCR import DeepTCR_WF
 
 #Train Sequence Classifier
-DTCR = DeepTCR_WF('TIL')
+DTCR = DeepTCR_WF('Human_TIL')
 dir = 'Topalian/beta/pre_crpr_sdpd'
 DTCR.Get_Data(directory='../../Data/Topalian',Load_Prev_Data=False,
                aa_column_beta=1,count_column=2,v_beta_column=7,d_beta_column=14,j_beta_column=21,data_cut=0.25,
--- ancillary_analysis/supervised/Supervised_Repertoire_Murine.py
+++ ancillary_analysis/supervised/Supervised_Repertoire_Murine.py
@@ -5,15 +5,15 @@ LOO = 4
 epochs_min = 50
 
 #Train Sequence Classifier
-DTCR_SS = DeepTCR_SS('Rudqvist')
+DTCR_SS = DeepTCR_SS('Rudqvist_SS')
 DTCR_SS.Get_Data(directory='../../Data/Rudqvist',Load_Prev_Data=False,
                aa_column_beta=1,count_column=2,v_beta_column=7,d_beta_column=14,j_beta_column=21)
 
 DTCR_SS.Monte_Carlo_CrossVal(folds=folds,test_size=0.25)
-DTCR_SS.AUC_Curve()
+DTCR_SS.AUC_Curve(filename='AUC.eps')
 
 #Train Repertoire Classifier without on-graph clustering
-DTCR_WF = DeepTCR_WF('Rudqvist')
+DTCR_WF = DeepTCR_WF('Rudqvist_WF')
 DTCR_WF.Get_Data(directory='../../Data/Rudqvist',Load_Prev_Data=False,
                aa_column_beta=1,count_column=2,v_beta_column=7,d_beta_column=14,j_beta_column=21)
 
