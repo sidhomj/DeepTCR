9abe3de55df441b083b930662cdb6f31d2178510
JW <sidhomj@gmail.com>
2019-04-23 20:02:09 -0400

    bug_fix

--- ancillary_analysis/supervised/Supervised_Repertoire_Murine.py
+++ ancillary_analysis/supervised/Supervised_Repertoire_Murine.py
@@ -1,11 +1,15 @@
 from DeepTCR.DeepTCR import DeepTCR_SS, DeepTCR_WF
 
+folds = 100
+LOO = 4
+epochs_min = 50
+
 #Train Sequence Classifier
 DTCR_SS = DeepTCR_SS('Rudqvist')
 DTCR_SS.Get_Data(directory='../../Data/Rudqvist',Load_Prev_Data=False,aggregate_by_aa=True,
                aa_column_beta=1,count_column=2,v_beta_column=7,d_beta_column=14,j_beta_column=21)
 
-DTCR_SS.Monte_Carlo_CrossVal(folds=100,test_size=0.25)
+DTCR_SS.Monte_Carlo_CrossVal(folds=folds,test_size=0.25)
 DTCR_SS.AUC_Curve()
 
 #Train Repertoire Classifier without on-graph clustering
@@ -13,11 +17,11 @@ DTCR_WF = DeepTCR_WF('Rudqvist')
 DTCR_WF.Get_Data(directory='../../Data/Rudqvist',Load_Prev_Data=False,aggregate_by_aa=True,
                aa_column_beta=1,count_column=2,v_beta_column=7,d_beta_column=14,j_beta_column=21)
 
-DTCR_WF.Monte_Carlo_CrossVal(folds=100,LOO=4,epochs_min=50)
+DTCR_WF.Monte_Carlo_CrossVal(folds=folds,LOO=LOO,epochs_min=epochs_min)
 DTCR_WF.AUC_Curve()
 
 #Train Repertoire Classifier with on-graph clustering
-DTCR_WF.Monte_Carlo_CrossVal(folds=100,LOO=4,on_graph_clustering=True,epochs_min=50)
+DTCR_WF.Monte_Carlo_CrossVal(folds=folds,LOO=LOO,on_graph_clustering=True,epochs_min=epochs_min)
 DTCR_WF.AUC_Curve()
 
 #Visualize Latent Space
