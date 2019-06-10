1a54ebb3911169d40e728cf17ddb78edfac47072
JW <sidhomj@gmail.com>
2019-06-10 13:53:39 -0400

    bug_fix

--- ancillary_analysis/supervised/Supervised_Repertoire_Murine.py
+++ ancillary_analysis/supervised/Supervised_Repertoire_Murine.py
@@ -2,23 +2,23 @@ from DeepTCR.DeepTCR import DeepTCR_SS, DeepTCR_WF
 
 folds = 100
 LOO = 4
-epochs_min = 100
+epochs_min = 10000
 
-#Train Sequence Classifier
-DTCR_SS = DeepTCR_SS('Rudqvist_SS')
-DTCR_SS.Get_Data(directory='../../Data/Rudqvist',Load_Prev_Data=False,
-               aa_column_beta=1,count_column=2,v_beta_column=7,d_beta_column=14,j_beta_column=21)
-
-DTCR_SS.Monte_Carlo_CrossVal(folds=folds,test_size=0.25)
-DTCR_SS.AUC_Curve(filename='AUC.eps')
+# #Train Sequence Classifier
+# DTCR_SS = DeepTCR_SS('Rudqvist_SS')
+# DTCR_SS.Get_Data(directory='../../Data/Rudqvist',Load_Prev_Data=False,
+#                aa_column_beta=1,count_column=2,v_beta_column=7,d_beta_column=14,j_beta_column=21)
+#
+# DTCR_SS.Monte_Carlo_CrossVal(folds=folds,test_size=0.25)
+# DTCR_SS.AUC_Curve(filename='AUC.eps')
 
 #Train Repertoire Classifier without on-graph clustering
 DTCR_WF = DeepTCR_WF('Rudqvist_WF')
 DTCR_WF.Get_Data(directory='../../Data/Rudqvist',Load_Prev_Data=False,
                aa_column_beta=1,count_column=2,v_beta_column=7,d_beta_column=14,j_beta_column=21)
 
-DTCR_WF.Monte_Carlo_CrossVal(folds=folds,LOO=LOO,epochs_min=epochs_min)
-DTCR_WF.AUC_Curve(filename='Rep_AUC.eps')
+# DTCR_WF.Monte_Carlo_CrossVal(folds=folds,LOO=LOO,epochs_min=epochs_min)
+# DTCR_WF.AUC_Curve(filename='Rep_AUC.eps')
 
 #Train Repertoire Classifier with on-graph clustering
 DTCR_WF.Monte_Carlo_CrossVal(folds=folds,LOO=LOO,on_graph_clustering=True,epochs_min=epochs_min)
