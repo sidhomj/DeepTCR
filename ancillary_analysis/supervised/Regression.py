b79e1cc3965f08d6db5a85da4782e4401b259bd2
JW <sidhomj@gmail.com>
2019-06-11 14:00:08 -0400

    bug_fix

--- ancillary_analysis/supervised/Regression.py
+++ ancillary_analysis/supervised/Regression.py
@@ -6,25 +6,28 @@ import pandas as pd
 import pickle
 from DeepTCR.DeepTCR import DeepTCR_SS
 import numpy as np
+from multiprocessing import Pool
 
-with open('../../Data/10x_Data/Data_Regression.pkl','rb') as f:
-    df = pickle.load(f)
+df = pd.read_csv('../../Data/10x_Data/Data_Regression.csv')
 
 DTCRS = DeepTCR_SS('reg')
+p = Pool(40)
 
 #Get alpha/beta sequences
 alpha = np.asarray(df['alpha'].tolist())
 beta = np.asarray(df['beta'].tolist())
+sample = np.asarray(df['Sample'].tolist())
 
 y_pred = []
 y_test = []
 antigen = []
 #Iterate through all antigens
-for i in range(2,df.columns.shape[0]):
+for i in range(3,df.columns.shape[0]):
+    print(df.iloc[:,i].name)
     sel = df.iloc[:,i]
     Y = np.log2(np.asarray(sel.tolist()) + 1)
-    DTCRS.Load_Data(alpha_sequences=alpha, beta_sequences=beta, Y=Y)
-    DTCRS.K_Fold_CrossVal(folds=5)
+    DTCRS.Load_Data(alpha_sequences=alpha, beta_sequences=beta, Y=Y,p=p,sample_labels=sample)
+    DTCRS.K_Fold_CrossVal(split_by_sample=True)
     y_pred.append(DTCRS.y_pred)
     y_test.append(DTCRS.y_test)
     antigen.append([sel.name]*len(DTCRS.y_pred))
