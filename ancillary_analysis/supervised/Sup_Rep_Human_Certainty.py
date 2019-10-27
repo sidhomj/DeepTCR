70878dc0d43fbe33593f0761d62869e26c39e8a2
JW <sidhomj@gmail.com>
2019-10-27 00:19:25 -0400

    tutorial_updates

---
+++ ancillary_analysis/supervised/Sup_Rep_Human_Certainty.py
@@ -0,0 +1,50 @@
+"""
+Supplementary Figure 19
+"""
+
+"""This script is used to generate the plot from the previously trained HLA, TCR, TCR+HLA
+models showing the certainty of the predictions for each sample for these models on the 
+CheckMate-038 clinical trial data. """
+
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+import pandas as pd
+from scipy.stats import wilcoxon, rankdata, ranksums
+
+# data files to process and their label
+model_info = {'TCR': dict(file='sample_tcr.csv', alpha=1.),
+              'HLA': dict(file='sample_hla.csv', alpha=1.),
+              'TCR+HLA': dict(file='sample_tcr_hla.csv', alpha=1.0)}
+
+# mc pred approach
+for model_name in model_info:
+    model_info[model_name]['mc_preds'] = pd.read_csv(model_info[model_name]['file'])
+    model_info[model_name]['mean'] = model_info[model_name]['mc_preds'].groupby('Samples')[['y_test', 'y_pred']].agg(np.mean)
+
+# mc_preds per sample distribution plot sorted by sample mean in combo model
+_, ax = plt.subplots(ncols=3)
+i = 0
+idx = model_info[model_name]['mean']['y_pred'].sort_values().index
+for model_name in model_info:
+    sns.violinplot(x=model_info[model_name]['mc_preds']['y_pred'].values, y=model_info[model_name]['mc_preds']['Samples'].values, linewidth=.5, order=idx, ax=ax[i], cut=0)
+    ax[i].set(yticklabels=[], xlim=[0, 1], title=model_name)
+    i += 1
+label_dict = {0: 'sdpd', 1: 'crpr'}
+ax[0].set(yticklabels=[label_dict[_] for _ in model_info[model_name]['mean'].loc[idx, ]['y_test'].values])
+ax[1].set(yticks=[])
+ax[2].set(yticks=[])
+plt.tight_layout()
+
+for model_name in model_info:
+    model_info[model_name]['mean']['90 CI'] = model_info[model_name]['mc_preds'].groupby('Samples')['y_pred'].apply(lambda x: np.diff(np.quantile(x, [.1, .9]))[0])
+
+# paired tests
+wilcoxon(model_info['TCR+HLA']['mean']['90 CI'], model_info['HLA']['mean']['90 CI'])
+wilcoxon(model_info['TCR+HLA']['mean']['90 CI'], model_info['TCR']['mean']['90 CI'])
+wilcoxon(model_info['TCR']['mean']['90 CI'], model_info['HLA']['mean']['90 CI'])
+
+# non paired
+ranksums(model_info['TCR+HLA']['mean']['90 CI'], model_info['HLA']['mean']['90 CI'])
+ranksums(model_info['TCR+HLA']['mean']['90 CI'], model_info['TCR']['mean']['90 CI'])
+ranksums(model_info['TCR']['mean']['90 CI'], model_info['HLA']['mean']['90 CI'])
--- ancillary_analysis/supervised/Supervised_Repertoire_Human.py
+++ ancillary_analysis/supervised/Supervised_Repertoire_Human.py
@@ -1,91 +1,94 @@
-"""Figure 3C
-Supplementary Figure 19
+"""
+Figure 3C
 """
 
-"""This script is used to generate the plot from the previously trained HLA, TCR, TCR+HLA
-models showing the comparative performance of these models on the CheckMate-038 clinical trial.."""
+"""This script is used to generate the ROC plot from the previously trained HLA, TCR, TCR+HLA
+models showing the comparative performance of these models on the CheckMate-038 clinical trial data."""
 
-import numpy as np
-from matplotlib import pyplot as plt
+from sklearn.metrics import roc_auc_score, roc_curve
+import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-from sklearn.metrics import roc_curve, auc
-from scipy.stats import wilcoxon, rankdata, ranksums
+import numpy as np
 
-# AUC plot axes
+#Load Data
+df_tcr = pd.read_csv('sample_tcr.csv')
+df_hla = pd.read_csv('sample_hla.csv')
+df_tcr_hla = pd.read_csv('sample_tcr_hla.csv')
+
+#Concat Data
+df = pd.DataFrame()
+df['Samples'] = df_tcr['Samples']
+df['TCR'] = df_tcr['y_pred']
+df['HLA'] = df_hla['y_pred']
+df['TCR+HLA'] = df_tcr_hla['y_pred']
+df['Label'] = df_tcr_hla['y_test']
+models = ['TCR','HLA','TCR+HLA']
+
+#Run Bootstrapping to assess differneces between
+n_boots=5000
+auc_list = []
+model_list = []
+for n in range(n_boots):
+    idx = np.random.choice(range(len(df)), len(df), replace=True)
+    auc_list.append(roc_auc_score(df['Label'].iloc[idx],df['TCR'].iloc[idx]))
+    model_list.append('TCR')
+    auc_list.append(roc_auc_score(df['Label'].iloc[idx],df['HLA'].iloc[idx]))
+    model_list.append('HLA')
+    auc_list.append(roc_auc_score(df['Label'].iloc[idx],df['TCR+HLA'].iloc[idx]))
+    model_list.append('TCR+HLA')
+
+df_bootstrap = pd.DataFrame()
+df_bootstrap['model'] = model_list
+df_bootstrap['auc'] = auc_list
+
+#Draw figure
 f, ax1 = plt.subplots()
-
-# data files to process and their label
-model_info = {'TCR': dict(file='sample_tcr.csv', alpha=1.),
-              'HLA': dict(file='sample_hla.csv', alpha=1.),
-              'TCR+HLA': dict(file='sample_tcr_hla.csv', alpha=1.0)}
-
-#
-# mc pred approach
-
-# number of boot straps
-n_boot = 5000
-for model_name in model_info:
-    model_info[model_name]['mc_preds'] = pd.read_csv(model_info[model_name]['file'])
-    model_info[model_name]['mean'] = model_info[model_name]['mc_preds'].groupby('Samples')[['y_test', 'y_pred']].agg(np.mean)
-    fpr, tpr, _ = roc_curve(model_info[model_name]['mc_preds'].y_test.values, model_info[model_name]['mc_preds'].y_pred.values)
-    model_info[model_name]['full'] = {'fpr': fpr, 'tpr': tpr, 'auc': auc(fpr, tpr)}
-    model_info[model_name]['bootstrap'] = {'auc': list()}
-
-for model_name in model_info:
-    ax1.plot(model_info[model_name]['full']['fpr'], model_info[model_name]['full']['tpr'], alpha=model_info[model_name]['alpha'], label='%s (%.2f)' % (model_name,  model_info[model_name]['full']['auc']))
-ax1.legend()
-
-for i in range(n_boot):
-    idx = np.random.choice(model_info['TCR']['mc_preds'].shape[0], model_info['TCR']['mc_preds'].shape[0],replace=True)
-
-    for model_name in model_info:
-        fpr, tpr, _ = roc_curve(model_info[model_name]['mc_preds']['y_test'].loc[idx].values, model_info[model_name]['mc_preds']['y_pred'].loc[idx].values)
-        model_info[model_name]['bootstrap']['auc'].append(auc(fpr, tpr))
-
-for model_name in model_info:
-    model_info[model_name]['bootstrap']['auc'] = np.array(model_info[model_name]['bootstrap']['auc'])
-
-ax2 = f.add_axes([0.48, .2, .4, .4])
-sns.violinplot(data=[model_info[model_name]['bootstrap']['auc'] for model_name in model_info], orient='v', cut=0, ax=ax2)
-ax2.set(xticklabels=model_info.keys(), frame_on=True)
-ax2.spines['right'].set_visible(False)
-ax2.spines['top'].set_visible(False)
+ax1.set_xlim([0.0, 1.0])
+ax1.set_ylim([0.0, 1.05])
 ax1.set_xlabel('False Positive Rate')
 ax1.set_ylabel('True Positive Rate')
+for m in models:
+    roc_score = roc_auc_score(df['Label'], df[m])
+    fpr, tpr, th = roc_curve(df['Label'], df[m])
+    class_name = m
+    ax1.plot(fpr, tpr, lw=2, label='%s (%0.2f)' % (class_name, roc_score))
+ax1.legend(loc='upper left',frameon=False)
 
-# 95 Bootstrap CI of test statistic - AUC, null hyopthesis is that diff = 0
-np.quantile(model_info['TCR+HLA']['bootstrap']['auc'] - model_info['HLA']['bootstrap']['auc'], [0.025, 0.975])
-np.quantile(model_info['TCR+HLA']['bootstrap']['auc'] - model_info['TCR']['bootstrap']['auc'], [0.025, 0.975])
-np.quantile(model_info['HLA']['bootstrap']['auc'] - model_info['TCR']['bootstrap']['auc'], [0.025, 0.975])
+ax2 = f.add_axes([0.48, .2, .4, .4])
+ax2.spines['right'].set_visible(False)
+ax2.spines['top'].set_visible(False)
+sns.violinplot(data=df_bootstrap,x='model',y='auc',ax=ax2,cut=0)
+ax2.set_xlabel('')
 
-# checking lower bounds of 99.9, 99, and 95 CIs to see where they cross 0
-np.quantile(model_info['TCR+HLA']['bootstrap']['auc'] - model_info['HLA']['bootstrap']['auc'], [0.0005, 0.005, 0.025])
-np.quantile(model_info['TCR+HLA']['bootstrap']['auc'] - model_info['TCR']['bootstrap']['auc'], [0.0005, 0.005, 0.025])
+#Get P-Values
+df_pval = pd.DataFrame()
 
-# mc_preds per sample distribution plot sorted by sample mean in combo model
-_, ax = plt.subplots(ncols=3)
-i = 0
-idx = model_info[model_name]['mean']['y_pred'].sort_values().index
-for model_name in model_info:
-    sns.violinplot(x=model_info[model_name]['mc_preds']['y_pred'].values, y=model_info[model_name]['mc_preds']['Samples'].values, linewidth=.5, order=idx, ax=ax[i], cut=0)
-    ax[i].set(yticklabels=[], xlim=[0, 1], title=model_name)
-    i += 1
-label_dict = {0: 'sdpd', 1: 'crpr'}
-ax[0].set(yticklabels=[label_dict[_] for _ in model_info[model_name]['mean'].loc[idx, ]['y_test'].values])
-ax[1].set(yticks=[])
-ax[2].set(yticks=[])
-plt.tight_layout()
+model_1 = []
+model_2 = []
+p_val = []
 
-for model_name in model_info:
-    model_info[model_name]['mean']['90 CI'] = model_info[model_name]['mc_preds'].groupby('Samples')['y_pred'].apply(lambda x: np.diff(np.quantile(x, [.1, .9]))[0])
+models_test = ['TCR','HLA']
+m1 = np.array(df_bootstrap[df_bootstrap['model']==models_test[0]]['auc'])
+m2 = np.array(df_bootstrap[df_bootstrap['model']==models_test[1]]['auc'])
+model_1.append(models_test[0])
+model_2.append(models_test[1])
+p_val.append(np.where(np.sort(m2 - m1)>=0)[0][0]/n_boots)
 
-# paired tests
-wilcoxon(model_info['TCR+HLA']['mean']['90 CI'], model_info['HLA']['mean']['90 CI'])
-wilcoxon(model_info['TCR+HLA']['mean']['90 CI'], model_info['TCR']['mean']['90 CI'])
-wilcoxon(model_info['TCR']['mean']['90 CI'], model_info['HLA']['mean']['90 CI'])
+models_test = ['TCR','TCR+HLA']
+m1 = np.array(df_bootstrap[df_bootstrap['model']==models_test[0]]['auc'])
+m2 = np.array(df_bootstrap[df_bootstrap['model']==models_test[1]]['auc'])
+model_1.append(models_test[0])
+model_2.append(models_test[1])
+p_val.append(np.where(np.sort(m2 - m1)>=0)[0][0]/n_boots)
 
-# non paired
-ranksums(model_info['TCR+HLA']['mean']['90 CI'], model_info['HLA']['mean']['90 CI'])
-ranksums(model_info['TCR+HLA']['mean']['90 CI'], model_info['TCR']['mean']['90 CI'])
-ranksums(model_info['TCR']['mean']['90 CI'], model_info['HLA']['mean']['90 CI'])
+models_test = ['HLA','TCR+HLA']
+m1 = np.array(df_bootstrap[df_bootstrap['model']==models_test[0]]['auc'])
+m2 = np.array(df_bootstrap[df_bootstrap['model']==models_test[1]]['auc'])
+model_1.append(models_test[0])
+model_2.append(models_test[1])
+p_val.append(np.where(np.sort(m2 - m1)>=0)[0][0]/n_boots)
+
+df_pval['Model_1'] = model_1
+df_pval['Model_2'] = model_2
+df_pval['P_Val'] = p_val
