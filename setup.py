c540eb208f4778f4f95842a6e5e246b85df7a431
JW <sidhomj@gmail.com>
2019-11-16 09:41:46 -0500

    tutorial_updates

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup, find_packages
 import sys
 
-
 if sys.version_info.major != 3:
     raise RuntimeError("DeepTCR requires Python 3")
 
@@ -11,7 +10,7 @@ with open('requirements.txt') as f:
 setup(
     name="DeepTCR",
     description="Deep Learning Methods for Parsing T-Cell Receptor Sequencing (TCRSeq) Data",
-    version="1.3.5",
+    version="1.3.9",
     author="John-William Sidhom",
     author_email="jsidhom1@jhmi.edu",
     packages=find_packages(),
