8e5b93406c931337c95d9b39638c7baacc7dcc15
JW <sidhomj@gmail.com>
2019-11-16 17:01:22 -0500

    tutorial_updates

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(req_file) as f:
 setup(
     name="DeepTCR",
     description="Deep Learning Methods for Parsing T-Cell Receptor Sequencing (TCRSeq) Data",
-    version="1.3.10",
+    version="1.3.11",
     author="John-William Sidhom",
     author_email="jsidhom1@jhmi.edu",
     packages=find_packages(),
