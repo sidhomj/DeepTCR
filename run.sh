aead7561509c2c0b756d348f40bdc64608cf37e1
JW <sidhomj@gmail.com>
2020-04-08 02:02:22 -0400

    features update

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Render the notebook to HTML
-jupyter nbconvert \
-  --ExecutePreprocessor.allow_errors=True \
-  --ExecutePreprocessor.timeout=-1 \
-  --output-dir=/results \
-  --execute Tutorial.ipynb
