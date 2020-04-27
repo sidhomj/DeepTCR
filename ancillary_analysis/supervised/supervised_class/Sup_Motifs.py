ea1e9a4e18d858c9f6de3dbf450e6c99890b86f0
JW <sidhomj@gmail.com>
2020-04-27 18:32:44 -0400

    update analyses

---
+++ ancillary_analysis/supervised/supervised_class/Sup_Motifs.py
@@ -0,0 +1,47 @@
+"""
+Fig 2C
+"""
+
+"""This script was used to train the supervised TCR sequence classifier
+and generate the top representative sequences for each class and derive the 
+motifs that were learned by the network."""
+
+from DeepTCR.DeepTCR import DeepTCR_SS
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC
+import numpy as np
+import os
+import shutil
+
+#Run Supervised Sequence Classifier
+DTCRS = DeepTCR_SS('Sequence_C',device=6)
+DTCRS.Get_Data(directory='../../Data/Murine_Antigens',Load_Prev_Data=True,aggregate_by_aa=True,
+               aa_column_beta=0,count_column=1,v_beta_column=2,j_beta_column=3)
+
+folds = 100
+seeds = np.array(range(folds))
+graph_seed = 0
+DTCRS.Monte_Carlo_CrossVal(folds=folds,graph_seed=graph_seed,seeds=seeds)
+DTCRS.Representative_Sequences(top_seq=25,motif_seq=10,color_scheme='hydrophobicity')
+
+dir = 'Murine_Rep_Sequences'
+if os.path.exists(dir):
+    shutil.rmtree(dir)
+os.makedirs(dir)
+
+for item in DTCRS.Rep_Seq:
+    t = DTCRS.Rep_Seq[item]
+    t = t.groupby(['beta']).agg({item:'first'})
+    t = t.sort_values(by=item,ascending=False)
+    t.reset_index(inplace=True)
+    seq = t['beta'].tolist()
+    seq = seq[:10]
+    out = []
+    for s in seq:
+        out.append(SeqRecord(Seq(s, IUPAC.protein), s))
+    SeqIO.write(out,os.path.join(dir,item+'.fasta'),'fasta')
+
+
+
---
+++ ancillary_analysis/supervised/supervised_class/Sup_Seq_ROC.py
@@ -0,0 +1,25 @@
+"""Figure 2B"""
+
+"""This script is used to create the ROC curves for assessing the ability
+of supervised sequence classifier to correctly predict the antigen-specificity of 
+the 9 murine antigens in the manuscript.."""
+
+from DeepTCR.DeepTCR import DeepTCR_SS
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rc('font', family='Arial')
+
+#Run Supervised Sequence Classifier
+DTCRS = DeepTCR_SS('Sequence_C',device=2)
+
+
+DTCRS.Get_Data(directory='../../Data/Murine_Antigens',Load_Prev_Data=False,aggregate_by_aa=True,
+               aa_column_beta=0,count_column=1,v_beta_column=2,j_beta_column=3)
+
+folds = 10
+seeds = np.array(range(folds))
+graph_seed = 0
+DTCRS.Monte_Carlo_CrossVal(folds=folds,seeds=seeds,graph_seed=graph_seed)
+DTCRS.AUC_Curve(xlabel_size=24,ylabel_size=24,xtick_size=18,ytick_size=18,legend_font_size=14,frameon=False,
+                diag_line=False)
