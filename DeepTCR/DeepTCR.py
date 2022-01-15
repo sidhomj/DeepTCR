a3ff8fe1d5c05f079a88e0add99849215ad4eb8c
sidhomj <jsidhom1@jhmi.edu>
2022-01-15 14:56:33 -0500

    bug_fix

--- DeepTCR/DeepTCR.py
+++ DeepTCR/DeepTCR.py
@@ -3997,23 +3997,23 @@ class DeepTCR_WF(DeepTCR_S_base):
                 GO.Features = Features
                 agg_list = []
                 if qualitative_agg:
-
-                    # #qualitative agg
-                    # GO.Features_W = Features * GO.X_Freq[:, tf.newaxis]
-                    # GO.Features_Agg = tf.sparse.sparse_dense_matmul(GO.sp, GO.Features_W)
-                    # #normalize
-                    # GO.Features_Agg = GO.Features_Agg / tf.sparse.sparse_dense_matmul(GO.sp, GO.X_Freq[:, tf.newaxis])
-                    # agg_list.append(GO.Features_Agg)
-                    #
-                    for _ in range(1):
-                        # GO.Features_Agg = dynamic_pooling(Features,GO.X_Freq[:, tf.newaxis],GO.sp,GO,num_iter=3)
-                        # GO.Features_Agg = context_pooling(Features,GO.X_Freq[:, tf.newaxis],GO.sp,GO,num_iter=3)
-                        attn_w = transformer_attn(Features,GO.X_Freq[:, tf.newaxis],GO.sp,GO,num_iter=3)
-                        GO.Features_W = attn_w*Features * GO.X_Freq[:, tf.newaxis]
+                    attn=True
+                    attn_heads=3
+                    if attn is False:
+                        #qualitative agg
+                        GO.Features_W = Features * GO.X_Freq[:, tf.newaxis]
                         GO.Features_Agg = tf.sparse.sparse_dense_matmul(GO.sp, GO.Features_W)
                         #normalize
                         GO.Features_Agg = GO.Features_Agg / tf.sparse.sparse_dense_matmul(GO.sp, GO.X_Freq[:, tf.newaxis])
                         agg_list.append(GO.Features_Agg)
+                    else:
+                        for _ in range(attn_heads):
+                            attn_w = transformer_attn(Features,GO.X_Freq[:, tf.newaxis],GO.sp,num_iter=3)
+                            GO.Features_W = attn_w*Features * GO.X_Freq[:, tf.newaxis]
+                            GO.Features_Agg = tf.sparse.sparse_dense_matmul(GO.sp, GO.Features_W)
+                            #normalize
+                            GO.Features_Agg = GO.Features_Agg / tf.sparse.sparse_dense_matmul(GO.sp, GO.X_Freq[:, tf.newaxis])
+                            agg_list.append(GO.Features_Agg)
 
                 if quantitative_agg:
                     #quantitative agg
