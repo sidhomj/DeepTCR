412ab9a12fce631a3d09756dd589f95ee1377939
sidhomj <jsidhom1@jhmi.edu>
2022-08-24 02:29:51 -0400

    update

--- ancillary_analysis/synapse/inference.py
+++ ancillary_analysis/synapse/inference.py
@@ -2,67 +2,35 @@ import pandas as pd
 import numpy as np
 from DeepTCR.DeepSynapse import DeepSynapse
 from DeepTCR.functions_syn.data_processing import Process_Seq, supertype_conv_op
+import utils
 
 df = pd.read_csv('../../Data/synapse/testing_data.csv')
 df['bind'] = True
 df['id'] = df['CDR3']+'_'+df['Antigen']+'_'+df['HLA']
-hla_list = np.array(df['HLA'].value_counts().index)
+dfs = utils.create_negative_samples(df,within_hla=True)
 
-# # shuffle across all HLA
-# dfs = []
-# for _ in range(100):
-#     df_shuffle = pd.DataFrame()
-#     df_shuffle['CDR3'] = np.random.choice(df['CDR3'],size=len(df),replace=False)
-#     # df_shuffle['CDR3'] = np.random.choice(bg['x'],size=len(df),replace=True)
-#     df_shuffle['Antigen'] = df['Antigen']
-#     df_shuffle['HLA'] = df['HLA']
-#     df_shuffle['id'] = df_shuffle['CDR3']+'_'+df_shuffle['Antigen']+'_'+df_shuffle['HLA']
-#     df_shuffle['bind'] = df_shuffle['id'].isin(df['id'])
-#     df_shuffle = df_shuffle[df_shuffle['bind'] != True]
-#     dfs.append(df_shuffle)
-#     dfs_temp = pd.concat(dfs)
-#     dfs_temp.drop_duplicates(inplace=True)
-#     if len(dfs_temp) > 10 * len(df):
-#         break
-# dfs = pd.concat(dfs)
-# dfs.drop_duplicates(inplace=True)
+#add labels
+df_input  = pd.concat([df,dfs])
+df_input['bind'] = df_input['bind'].astype(int)
+df_input['bind_cat'] = None
+df_input['bind_cat'][df_input['bind']==1] = 'bind'
+df_input['bind_cat'][df_input['bind']!=1] = 'non-bind'
 
-#shuffle within HLA types
-dfs = []
-for h in hla_list:
-    df_sel = df[df['HLA']==h]
-    df_sel.reset_index(drop=True,inplace=True)
-    dfs_temp = []
-    for _ in range(100):
-        df_shuffle = pd.DataFrame()
-        df_shuffle['CDR3'] = np.random.choice(df_sel['CDR3'], size=len(df_sel), replace=False)
-        df_shuffle['Antigen'] = df_sel['Antigen']
-        df_shuffle['HLA'] = df_sel['HLA']
-        df_shuffle['id'] = df_shuffle['CDR3'] + '_' + df_shuffle['Antigen'] + '_' + df_shuffle['HLA']
-        df_shuffle['bind'] = df_shuffle['id'].isin(df_sel['id'])
-        df_shuffle = df_shuffle[df_shuffle['bind'] != True]
-        dfs_temp.append(df_shuffle)
-        dfs_temp2 = pd.concat(dfs_temp)
-        dfs_temp2.drop_duplicates(inplace=True)
-        if len(dfs_temp2) > 10 * len(df_sel):
-            break
-    dfs.append(dfs_temp2)
-dfs = pd.concat(dfs)
-dfs.drop_duplicates(inplace=True)
+#process seq & hla
+df_input = Process_Seq(df_input,'CDR3')
+df_input = Process_Seq(df_input,'Antigen')
+df_input['HLA'] = df_input['HLA'].str.replace('*',"")
+df_input['HLA'] = df_input['HLA'].str.replace(':',"")
+df_input['HLA'] = df_input['HLA'].str[0:5]
 
-df_train  = pd.concat([df,dfs])
-df_train['bind'] = df_train['bind'].astype(int)
-df_train['bind_cat'] = None
-df_train['bind_cat'][df_train['bind']==1] = 'bind'
-df_train['bind_cat'][df_train['bind']!=1] = 'non-bind'
+#convert supertypes to alleles
+hla_supertype = pd.read_csv('../../DeepTCR/library/Supertype_Data_Dict.csv')
+hla_supertype = hla_supertype[['Supertype_2','Allele']]
+hla_supertype.drop_duplicates(inplace=True,subset=['Supertype_2'])
+hla_dict = dict(zip(hla_supertype['Supertype_2'],hla_supertype['Allele']))
+df_input['HLA'] = df_input['HLA'].map(hla_dict).fillna(df_input['HLA'])
+df_input = df_input[df_input['HLA'].str.len()==5]
 
-df_train = Process_Seq(df_train,'CDR3')
-df_train = Process_Seq(df_train,'Antigen')
-df_train['HLA'] = df_train['HLA'].str.replace('*',"")
-df_train['HLA'] = df_train['HLA'].str.replace(':',"")
-df_train['HLA'] = df_train['HLA'].str[0:5]
-df_train = df_train[df_train['HLA'].str.len()==5]
-df_train['HLA_sup'] = supertype_conv_op(df_train['HLA'],keep_non_supertype_alleles=True)
+df_input['HLA_sup'] = supertype_conv_op(df_input['HLA'],keep_non_supertype_alleles=True)
 
 DTCR = DeepSynapse('epitope_tcr')
-DTCR
