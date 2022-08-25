1cc0810ce13b0d6c96ccb1105db340f347edfd91
sidhomj <jsidhom1@jhmi.edu>
2022-08-25 01:22:52 -0400

    update

--- ancillary_analysis/synapse/foreign_v_self.py
+++ ancillary_analysis/synapse/foreign_v_self.py
@@ -12,10 +12,5 @@ DTCR.Load_Data(epitope_sequences = np.array(df['AA Sequence']),
                 class_labels=np.array(df['Y']),
                )
 
-DTCR.Monte_Carlo_CrossVal(folds=1,batch_size=50000,epochs_min=10,
-                          num_fc_layers=3,units_fc=256,
-                          units_hla=[12,12,12],kernel_hla=[30,30,30],stride_hla=[5,5,5])
-
-DTCR.SRCC()
-threshold = 1 - np.log10(500) / np.log10(50000)
-DTCR.AUC_Curve_CV(threshold=threshold)
+DTCR.Monte_Carlo_CrossVal(folds=1,batch_size=50000,epochs_min=10)
+DTCR.AUC_Curve()
